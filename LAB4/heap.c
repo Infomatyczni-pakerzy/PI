cf90e3d441b7f4372fc38c693f5bc83a05551c7e
wojkly <62295536+wojkly@users.noreply.github.com>
2020-03-26 18:03:47 +0100

    Add files via upload

---
+++ LAB4/heap.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+ int parent(int x)
+ {
+     return (x-1)/2;
+ }
+ int left(int x)
+ {
+     return 2*x + 1;
+ }
+ int right(int x)
+ {
+     return 2*x + 2;
+ }
+ void swap(int *a, int i, int j)
+ {
+     int x = a[i];
+     a[i] = a[j];
+     a[j] = x;
+ }
+void heapify(int *a, int i, int n)
+{
+    int l = left(i);
+    int r = right(i);
+    int largest = i;
+    if (l < n && a[l] > a[largest])
+    {
+        largest = l;
+    }
+    if (r < n && a[r] > a[largest])
+    {
+        largest = r;
+    }
+    if (largest != i)
+    {
+        swap(a,largest,i);
+        heapify(a,largest,n);
+    }
+
+}
+
+void heapSort(int *a, int n)
+{
+    for(int i = n/2; i>-1; i--)
+    {
+        heapify(a,i,n);
+    }
+    for(int i = n-1; i>0; i--)
+    {
+
+        swap(a,i,0);
+        heapify(a,0,i);
+    }
+}
+
+int main() {
+    int Z;
+    scanf("%d", &Z);
+
+	while (Z--) {
+		int n;
+		scanf("%d", &n);
+		int *a = malloc(n * sizeof(int));
+		for(int i = 0; i < n; i++) {
+			scanf("%d", &a[i]);
+		}
+
+        heapSort(a, n);
+        for(int i = 0; i < n; i++) {
+            printf("%d\n", a[i]);
+        }
+        free(a);
+    }
+}
+
---
+++ LAB4/quickSort.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void swap(int *a, int i, int j)
+{
+     int x = a[i];
+     a[i] = a[j];
+     a[j] = x;
+}
+int partition(int *a, int p, int r)
+{
+    int pivot = a[r];
+    int i = p-1;
+    for(int j=p; j<r; j++)
+    {
+        if (a[j] <= pivot)
+        {
+            i++;
+            swap(a, i, j);
+        }
+    }
+    i++;
+    swap(a, i, r);
+    return i;
+}
+
+void quickSort(int *a, int p, int r)
+{
+    if (p < r)
+    {
+        int q = partition(a,p,r);
+        quickSort(a, p, q-1);
+        quickSort(a, q+1, r);
+
+    }
+}
+
+int main() {
+    int Z;
+    scanf("%d", &Z);
+
+	while (Z--) {
+		int n;
+		scanf("%d", &n);
+		int *a = malloc(n * sizeof(int));
+		for(int i = 0; i < n; i++) {
+			scanf("%d", &a[i]);
+		}
+
+        quickSort(a, 0, n-1);
+        for(int i = 0; i < n; i++) {
+            printf("%d\n", a[i]);
+        }
+        free(a);
+    }
+}
